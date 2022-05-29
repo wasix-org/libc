e41762fbb43544f13ca6cdb63e970cbf2bdc005e
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2022-05-29 11:15:12 +0200

    Apparently I reexported from the wrong place - its the standard library that needs a re-export so did a rollback

--- src/lib.rs
+++ src/lib.rs
@@ -157,11 +157,6 @@ cfg_if! {
 
         mod wasi;
         pub use wasi::*;
-
-        // for backwards compatibility
-        pub mod unix {
-            pub use super::wasi::*;
-        }
     } else {
         // non-supported targets: empty...
     }
