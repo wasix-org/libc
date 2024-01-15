26f165f162250ad2fc3c6560103eb4e6d7384bce
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2024-01-15 19:50:24 +1100

    Removed the polyfill again

--- src/wasi.rs
+++ src/wasi.rs
@@ -1216,13 +1216,15 @@ extern "C" fn default_handler(sig: ::c_int) {
     }
 }
 
+/*
 #[cfg(target_vendor = "wasmer")]
 mod wasm_signal {
     #[no_mangle]
-    extern "C" fn __polyfill_for_wasm_signal(signum: ::c_int) {
+    extern "C" fn __wasm_signal_polyfill(signum: ::c_int) {
         unsafe { super::__wasm_signal(signum) };
     }
 }
+*/
 
 /// mocked functions that dont do anything in WASI land
 pub fn mlock(_addr: *const ::c_void, _len: ::size_t) -> ::c_int {
